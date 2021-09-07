1da73413caf0fab17ab2e43d360ed2a1288a4409
duajaradat <duajaradat164@gmail.com>
2021-09-07 12:16:50 +0300

    doen

--- src/components/AllDataAPI.js
+++ src/components/AllDataAPI.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
 import { withAuth0 } from '@auth0/auth0-react';
-
+import axios from 'axios';
+import { Card, Button } from 'react-bootstrap'
 
 class AllDataAPI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            apiDataArray: [],
+        }
+    }
+
+    componentDidMount = async () => {
+        const { user, isAuthenticated } = this.props.auth0;
+        let apiData = await axios.get(`${process.env.REACT_APP_SERVER}/dataApi`);
+        this.setState({
+            apiDataArray: apiData.data
+        }, () => { console.log(this.state.apiDataArray) });
+
+    }
+    addData = async (photoObj) => {
+        const { user, isAuthenticated } = this.props.auth0;
+        let addingData = await axios.post(`${process.env.REACT_APP_SERVER}/addtomyfav/${user.email}`, photoObj)
+        this.setState({
+            apiDataArray: addingData.data
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>All Data from the API</h1>
                 <h3>Select your favorites :)</h3>
+                {this.state.apiDataArray.map((photoObj, i) => {
+                    return (
+                        <Card key={i} style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={photoObj.image} />
+                            <Card.Body>
+                                <Card.Title>{photoObj.name}</Card.Title>
+                                <Button onClick={() => { this.addData(photoObj) }} variant="primary">Add to MyFav</Button>
+                            </Card.Body>
+                        </Card>
+                    )
+                })}
             </div>
         )
     }
--- src/components/MyFavorites.js
+++ src/components/MyFavorites.js
@@ -2,15 +2,61 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '././MyFavorites.js';
 import { withAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
+import { Card, Button } from 'react-bootstrap';
 
 class MyFavorites extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      myFavorites: [],
+    }
+  }
+  componentDidMount = async () => {
+    const { user, isAuthenticated } = this.props.auth0;
+    console.log('yyyyyyyyyyyyyyyyy')
+    // http://localhost:3003/myfav/:email
+    if (isAuthenticated) {
+      let myfavData = await axios.get(`${process.env.REACT_APP_SERVER}/myfav/${user.email}`);
+      this.setState({
+        myFavorites: myfavData.data
+      }, () => { console.log(this.state.myFavorites) })
+
+    }
+
+  }
+
+  // /deletefrommyfav/:photoId
+  deleteFromMyFav = async (photoId) => {
+    const { user, isAuthenticated } = this.props.auth0;
+    if (isAuthenticated) {
+      let deletemyfavData = await axios.delete(`${process.env.REACT_APP_SERVER}/deletefrommyfav/${photoId}?email=${user.email}`);
+      let myFavorites = deletemyfavData.data;
+      this.setState({
+        myFavorites: myFavorites
+      })
+    }
+  }
+
   render() {
-    return(
+    return (
       <>
         <h1>My Favorites</h1>
         <p>
           This is a collection of my favorites
         </p>
+        {this.state.myFavorites.map((Obj, i) => {
+          return (
+            <Card key={i} style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={Obj.image} />
+              <Card.Body>
+                <Card.Title>{Obj.name}</Card.Title>
+                <Button onClick={() => { this.deleteFromMyFav(Obj._id) }} variant="primary">Delete</Button>
+                <Button variant="primary">Update</Button>
+              </Card.Body>
+            </Card>
+          )
+        })}
       </>
     )
   }
